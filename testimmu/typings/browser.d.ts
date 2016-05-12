c17780beb3690240448505e7cd2dc1913ee3c3ed
z.w <charlenezell@gmail.com>
2016-05-12 19:29:14 +0800

    remove typings and node_modules

--- testimmu/typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/chai/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/immutable/index.d.ts" />
--- testimmu/typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/definitions/immutable/index.d.ts" />
