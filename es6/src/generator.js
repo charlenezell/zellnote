2fe2d14ec29d4c0e35710962c27a3f25ad6f64dc
charlenezell <charlenezell@gmail.com>
2017-04-01 18:05:33 +0800

    end learning generator found jscsp,koa,and int- resting topic about FRP and CSP and EventBase

--- es6/src/generator.js
+++ es6/src/generator.js
@@ -1,3 +1,6 @@
+/// <reference path="../typings/index.d.ts" />
+let Q = require("Q");
+let co = require("co");
 let toRun = [{
     name: "basic",
     fn() {
@@ -50,6 +53,7 @@ let toRun = [{
             yield 5;
             return 6;
         }
+
         function* b(x) {
             yield 1;
             yield 2;
@@ -58,18 +62,271 @@ let toRun = [{
             yield 4;
             yield 5;
         }
-        for (let b of a()){
+        for (let b of a()) {
             console.log(b)
         }
         console.log("..........")
-        for (let c of b()){
+        for (let c of b()) {
             console.log(c)
         }
+    }
+}, {
+    name: "error Handling",
+    fn() {
+        function* a(x) {
+            console.log(1);
+            yield 1;
+            console.log(2);
+            try {
+                yield 2;
+            } catch (e) {
+                if (e == "simple") {
+                    console.log("catchThrow in generator", e);
+                } else {
+                    throw new Error("tohard generator cant deal with it");
+                }
+            }
+            console.log(3);
+            try {
+                yield 3;
+            } catch (e) {
+                if (e == "simple") {
+                    console.log("catchThrow in generator", e);
+                } else {
+                    throw new Error("tohard generator cant deal with it");
+                }
+            }
+            console.log(4);
+            yield 4;
+            console.log(5);
+            yield 5;
+            console.log(6);
+            return 6;
+
+
+        }
+        console.log("normalRun");
+        for (let c of a()) {}
+        console.log("errorRun");
+        let b = a();
+        b.next();
+        b.next(); //这里像是输入一个标准空的流
+        try {
+            b.throw("simple"); //这里更像是输入一个错误流
+        } catch (e) {
+            console.log("catch error myself", e);
+        }
+        b.next();
+        try {
+            b.throw("notSimple"); //这里更像是输入一个错误流
+        } catch (e) {
+            console.log("catch error myself", e);
+        }
+
+    }
+}, {
+    name: "next and for the rest",
+    fn() {
+        function* a(x) {
+            console.log("1");
+            yield 1;
+            console.log("2");
+            yield 2;
+            console.log("3");
+            yield 3;
+            console.log("4");
+            yield 4;
+        }
+        let b = a();
+        b.next();
+        b.next();
+        for (let c of b) {
+
+        }
+    }
+}, {
+    name: "next and for the rest with error",
+    fn() {
+        function* a(x) {
+            console.log("1");
+            try {
+                yield 1;
+            } catch (e) {
+                console.log("catched");
+            }
+            console.log("2");
+            yield 2;
+            console.log("3");
+            yield 3;
+            console.log("4");
+            yield 4;
+        }
+        let b = a();
+        b.next();
+        b.throw('');
+        try {
+            b.throw('');
+        } catch (e) {
+            console.log("catchoutside");
+        } finally { //这里似乎是外层接到的错误是不能finally掉的。。。。
+
+            for (let c of b) {
+
+            }
+        }
+
+    }
+}, {
+    name: "yield and yield and... return a msg from delegeted generator",
+    fn() {
+        function* a(x) {
+            yield 1;
+            yield 2;
+            let ok = yield* b();
+            console.log(ok);
+            yield 5;
+        }
+
+        function* b() {
+            yield 3;
+            yield 4;
+            return "end"
+        }
+        for (let c of a()) {
+            console.log(c);
+        }
+    }
+}, {
+    name: "async",
+    run: false,
+    fn() {
+        function dosomeasync(c) {
+            setTimeout(() => {
+                c("done");
+            }, 2000);
+        }
+
+        function normalCallback(cb) {
+            dosomeasync(function (d) {
+                return cb(d);
+            })
+        }
+
+        function doAPromise() {
+            return new Promise((res, rej) => {
+                dosomeasync((d) => {
+                    res(d);
+                })
+            });
+        }
+
+
+        function* doAGenerator() {
+            let rst = yield dosomeasync(function (d) {
+                g.next(d);
+            });
+            console.log(`${rst} with generator`);
+        }
+
+        let g = doAGenerator();
+        g.next();
+
+
+        normalCallback(function (v) {
+            console.log(`${v} with normalCallback`);
+        });
+
+        doAPromise().then(v => {
+            console.log(`${v} with promise`);
+        });
+    }
+}, {
+    name: "auto Wrapup",
+    fn() {
+        function dosomeasync(data) {
+            return new Promise((r, j) => {
+                setTimeout(() => {
+                    r(data);
+                }, 2000);
+            })
+
+        }
+
+        function runGen(gen) {
+            let g = gen();
+            let rst, end = false;
+
+            (function rec(value) {
+                rst = g.next(value);
+                end = rst.done;
+                if (end != true) {
+                    rst.value.then(rec);
+                }
+            })();
+
+            // for (let a of gen()){
+
+            // }
+        }
+
+        runGen(function* () {
+            let [a, b, c] = yield Promise.all([dosomeasync(1), dosomeasync(3), dosomeasync(5)]);
+            console.log(a, b, c);
+            let r2 = yield dosomeasync(2);
+            console.log(r2);
+        });
+    },
+    run: false
+}, {
+    name: "using library",
+    // run:false,
+    fn() {
+        function dosomeasync(data) {
+            return new Promise((r, j) => {
+                setTimeout(() => {
+                    if (data == 3) {
+                        j(data);
+                    } else {
+                        r(data);
+                    }
+                }, 2000);
+            })
+
+        }
+        co(function* () {
+            try {
+                let [a, b, c] = yield Promise.all([dosomeasync(1), dosomeasync(3), dosomeasync(5)]);
+                console.log(a, b, c);
+                let r2 = yield dosomeasync(2);
+                console.log(r2);
+            } catch (e) {
+                console.log("rejected and clearnUp transition");
+            }
+            let c = yield dosomeasync("end");
+            console.log(c);
+
+        });
+        // Q.spawn(function* () {
+        //     try {
+        //         let [a, b, c] = yield Promise.all([dosomeasync(1), dosomeasync(3), dosomeasync(5)]);
+        //         console.log(a, b, c);
+        //         let r2 = yield dosomeasync(2);
+        //         console.log(r2);
+        //     } catch (e) {
+        //         console.log("rejected and clearnUp transition");
+        //     }
+        //     let c = yield dosomeasync("end");
+        //     console.log(c);
+
+        // })
+
     }
 }]
 
 toRun.forEach(v => {
-    console.log(`++++++++++++++start '${v.name}'+++++++++++++`)
-    v.fn();
-    console.log(`--------------end '${v.name}'-------------`)
+    if (v.run === void(0) || v.run == true) {
+        console.log(`++++++++++++++start '${v.name}'+++++++++++++`)
+        v.fn();
+        console.log(`--------------end '${v.name}'-------------`)
+    }
 })
