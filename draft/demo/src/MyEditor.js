e72b01ad02981949787845e6a388c6a213ecd6ee
unknown <huangguangyao@rd.com>
2017-09-28 19:54:35 +0800

    testdraft

---
+++ draft/demo/src/MyEditor.js
@@ -0,0 +1,17 @@
+import React, {
+    Component
+} from 'react';
+
+import { Editor, EditorState } from 'draft-js';
+
+export default class MyEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+
+        }
+    }
+    render() {
+        return <div>hello</div>
+    }
+}
